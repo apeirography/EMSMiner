d8a8fd0324c099ec0910ea9c00373a88c342726e
Daïm, the Apeirographer <55509149+apeirography@users.noreply.github.com>
2020-05-19 15:25:25 -0400

    Implemented Base Functionality
    Mines ectocopial Mandelbrot seeds within the console argument specified bathymetric range.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EMSMiner
  Ectocopial Mandelbrot Seed Miner
+
+ Console program to efficiently mine Ectocopial Mandelbrot Seeds for the generation of Anthropobrots and/or Buddhabrots.
+ 
+ See [Anthropobrots](http://apeirography.art/en/projects/anthropobrots/) @ [Apeirography.art](http://www.aperography.art/) and [The Multitude Behind the Buddhabrot](https://rxiv.org/pdf/1604.0392v1.pdf) @ [viXra.org](http://rxiv.org/)
+ 
---
+++ main.go
@@ -0,0 +1,317 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"math"
+	"math/cmplx"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func main() {
+
+	min := flag.Int("min", 100, "minimum depth of seeds to mine")
+	max := flag.Int("max", 1000, "maximum depth of seeds to mine")
+	howmany := flag.Int("howmany", 1000000, "number of seeds to mine")
+	flag.Parse()
+
+	flag.Usage()
+
+	fmt.Println("")
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	seeds, realmin, realmax := Mine(*howmany, *min, *max)
+	SaveEMSFile(seeds, realmin, realmax)
+}
+
+// .EMS file handling
+
+func SaveEMSFile(seeds seedpack, min, max int) {
+	buf := new(bytes.Buffer)
+	buf.Reset()
+
+	seeds = seeds.Sort()
+
+	for _, c := range seeds {
+		binary.Write(buf, binary.LittleEndian, c)
+	}
+
+	md5 := md5.Sum(buf.Bytes())
+
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	outfilename := filepath.Join(dir, strconv.Itoa(min)+"-"+strconv.Itoa(max)+"_"+fmt.Sprintf("%x", string(md5[:]))+".ems")
+	outfile, err := os.OpenFile(outfilename, os.O_RDWR|os.O_CREATE, 0644)
+	fmt.Println(err)
+
+	outfile = outfile
+
+	buf.Reset()
+	binary.Write(buf, binary.LittleEndian, []byte("@DM.EMS{codex.apeirography.art} "))
+	for _, c := range seeds {
+		binary.Write(buf, binary.LittleEndian, c)
+	}
+	outfile.Write(buf.Bytes())
+
+	return
+}
+
+// Optimized Mining Function
+
+func Mine(howmany, min, max int) (seedpack, int, int) {
+
+	/**** Initialization ****/
+
+	if howmany < 1 {
+		panic("Number of seeds sought is less than one.")
+	}
+
+	if max < min {
+		panic("Maximum seed depth is less than minimum seed depth.")
+	}
+
+	if min < 2 {
+		panic("Minimum seed depth is less than 2.")
+	}
+
+	seeds := NewSeedpack(howmany)
+	sidx := 0
+	guidemap := GenerateGuidemap(51)
+	found := 0
+	relfound := 0
+
+	realmin, realmax := max, min
+
+	startTime := time.Now()
+	relstartTime := time.Now()
+	updateInterval := 1
+	fmt.Println("Commencing mining of seeds with depths between "+strconv.Itoa(min)+" - "+strconv.Itoa(max)+":")
+
+	b := 2.00 * 2.00
+
+	var z, c, oldz complex128
+	var l, i, j int
+	var repcheck, repcheckstart int
+
+	/**** Outer Loop Begins ****/
+	j = 0
+CheckNewC:
+
+	z = complex(0, 0)
+	c = complex(rand.Float64()*4-2, rand.Float64()*2)
+	l = max + 2
+	i = 0
+	repcheckstart = 2
+	repcheck = repcheckstart
+	oldz = z
+
+	/**** Inner Loop Begins ****/
+	i = 0
+IterateZ:
+	z = z*z + c
+	if repcheck == 0 {
+		if oldz == z {
+			i = -1
+			goto IterateZDone
+		}
+		oldz = z
+		if i%8 == 0 {
+			repcheckstart = repcheckstart + 2
+			if !guidemap.Check(c) && i%64 != 0 {
+				i = -1
+				goto IterateZDone
+			}
+		} else {
+			repcheckstart = repcheckstart + 1
+		}
+		repcheck = repcheckstart
+	}
+	repcheck--
+
+	i++
+	if i < l && (real(z)*real(z))+(imag(z)*imag(z)) <= b {
+		goto IterateZ
+	}
+	/**** Inner Loop Ceases ****/
+
+IterateZDone:
+	if i >= min && i <= max {
+		if i < realmin {
+			realmin = i
+		}
+		if i > realmax {
+			realmax = i
+		}
+		found++
+		relfound++
+		seeds[sidx] = c
+		sidx++
+		guidemap.Mark(c)
+		if relfound % updateInterval == 0 {
+			if time.Since(relstartTime).Seconds() < 45 {
+				if updateInterval > 5 && time.Since(relstartTime).Seconds() > 0 {
+					updateInterval = updateInterval * int(float64(90)/float64(time.Since(relstartTime).Seconds()))
+				}
+				updateInterval++
+				relfound = 0
+				relstartTime = time.Now()
+			} else {
+				hours := int(math.Floor(time.Since(startTime).Seconds())) / 3600
+				minutes := (int(math.Floor(time.Since(startTime).Seconds())) - (hours * 3600)) / 60
+				seconds := int(math.Floor(time.Since(startTime).Seconds())) - (hours * 3600) - (minutes * 60)
+				fmt.Println("After " + strconv.Itoa(hours) +"h "+strconv.Itoa(minutes)+"m " +strconv.Itoa(seconds) +"s, "+ strconv.Itoa(found) + " seeds with depths between "+strconv.Itoa(min) + " - " + strconv.Itoa(max)+" have been found so far. ("+strconv.Itoa(int(math.Round(float64(found)/time.Since(startTime).Seconds())))+" sps)")
+				relfound = 0
+				relstartTime = time.Now()
+			}
+		}
+	}
+
+	j++
+	if found < howmany {
+		goto CheckNewC
+	}
+	/**** Outer Loop Ceases ****/
+
+	hours := int(math.Floor(time.Since(startTime).Seconds())) / 3600
+	minutes := (int(math.Floor(time.Since(startTime).Seconds())) - (hours * 3600)) / 60
+	seconds := int(math.Floor(time.Since(startTime).Seconds())) - (hours * 3600) - (minutes * 60)
+	fmt.Println("After " + strconv.Itoa(hours) +"h "+strconv.Itoa(minutes)+"m " +strconv.Itoa(seconds) +"s, "+ strconv.Itoa(found) + " seeds with depths between "+strconv.Itoa(min) + " - " + strconv.Itoa(max)+" have been found so far. ("+strconv.Itoa(int(math.Round(float64(found)/time.Since(startTime).Seconds())))+" sps)")
+
+	return seeds, realmin, realmax
+}
+
+// Seedpack
+
+type seedpack []complex128
+
+func NewSeedpack(howmany int) seedpack {
+	return seedpack(make([]complex128, howmany))
+}
+
+func (this seedpack) Sort() seedpack {
+	sort.SliceStable(this, func(i, j int) bool {
+		if real(this[i]) != real(this[j]) {
+			return real(this[i]) < real(this[j])
+		} else {
+			return imag(this[i]) < imag(this[j])
+		}
+	})
+	return this
+}
+
+// Guidemap
+
+type Guidemap struct {
+	itsWidth, itsHeight int
+	itsMinR, itsMaxR float64
+	itsMinI, itsMaxI float64
+	itsDelR, itsDelI float64
+	itsData []bool
+}
+
+func GenerateGuidemap(size int) *Guidemap {
+
+	fmt.Print("Generating guidemap... ")
+
+	this := new(Guidemap)
+
+	this.itsWidth = size
+	this.itsHeight = size
+
+	this.itsMinR, this.itsMaxR = -2.00, 2.00
+	this.itsMinI, this.itsMaxI = -2.00, 2.00
+
+	this.itsDelR = (this.itsMaxR - this.itsMinR) / float64(this.itsWidth)
+	this.itsDelI = (this.itsMaxI - this.itsMinI) / float64(this.itsHeight)
+
+	this.itsData = make([]bool, this.itsWidth * this.itsHeight)
+
+	for idx := 0; idx < len(this.itsData); idx++ {
+		this.itsData[idx] = false
+	}
+
+	startTime := time.Now()
+	found := 0
+	limmin := 128
+	limmax := limmin * 2
+	for time.Since(startTime).Seconds() < 5 {
+
+		z := complex(0.00, 0.00)
+		c := complex(rand.Float64()*4-2, rand.Float64()*2)
+
+		for idx := 0; idx < limmax; idx++ {
+			z = z*z + c
+			if cmplx.Abs(z) > 2 {
+				if idx >= limmin {
+					found++
+					if found % 256 == 0 {
+						limmax *= 2
+					}
+					this.Mark(c)
+				}
+				break
+			}
+		}
+	}
+
+	fmt.Println("done.")
+
+	/*
+	for idx := 0; idx < len(this.itsData); idx++ {
+		if idx % this.itsWidth == 0 {
+			fmt.Print("\n")
+		}
+		if this.itsData[idx] {
+			fmt.Print("O")
+		} else {
+			fmt.Print("-")
+		}
+	}
+	fmt.Print("\n")
+	*/
+
+	return this
+}
+
+func (this *Guidemap) Mark(c complex128) {
+	x := int(math.Round((real(c) - this.itsMinR) / this.itsDelR))
+	y := int(math.Round((imag(c) - this.itsMinI) / this.itsDelI))
+	if x < 0 {
+		x = 0
+	}
+	if x > this.itsWidth-1 {
+		x = this.itsWidth - 1
+	}
+	if y < 0 {
+		y = 0
+	}
+	if y > this.itsHeight-1 {
+		y = this.itsHeight - 1
+	}
+	this.itsData[y*this.itsWidth+x] = true
+}
+
+func (this *Guidemap) Check(c complex128) bool {
+	x := int(math.Round((real(c) - this.itsMinR) / this.itsDelR))
+	y := int(math.Round((imag(c) - this.itsMinI) / this.itsDelI))
+	if x < 0 {
+		x = 0
+	}
+	if x > this.itsWidth-1 {
+		x = this.itsWidth - 1
+	}
+	if y < 0 {
+		y = 0
+	}
+	if y > this.itsHeight-1 {
+		y = this.itsHeight - 1
+	}
+	return this.itsData[y*this.itsWidth+x]
+}
