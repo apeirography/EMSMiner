2b57ae8ec2c64437f04f7157fad9f5f1090f8885
Daïm, the Apeirographer <55509149+apeirography@users.noreply.github.com>
2020-05-19 17:01:31 -0400

    Corrected licensing information.
    Removed boilerplate instructions from LICENSE file, and added copyright and licensing information to both source code and console output.

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+/*****************************************************************************
+ *  EMSMiner mines ectocopial Mandelbrot seeds used to create Anthropobrots. *
+ *  Copyright (C) 2020 Daïm Aggott-Hönsch                                    *
+ *                                                                           *
+ *  This program is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by     *
+ *  the Free Software Foundation, either version 3 of the License, or        *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+
 import (
 	"bytes"
 	"crypto/md5"
@@ -22,6 +40,10 @@ func main() {
 	max := flag.Int("max", 1000, "maximum depth of seeds to mine")
 	howmany := flag.Int("howmany", 1000000, "number of seeds to mine")
 	flag.Parse()
+
+	fmt.Println("\nEMSMiner Copyright (C) 2020 Daïm Aggott-Hönsch. This program comes with ABSOLUTELY NO WARRANTY.")
+	fmt.Println("This is free software, and you are welcome to redistribute it under the conditions specified by")
+	fmt.Println("the GNU General Public License 3 (https://www.gnu.org/licenses/gpl-3.0).")
 
 	fmt.Println("\nUsage: " + filepath.Base(os.Args[0]) + " -min [minimum_depth] -max [maximum_depth] -howmany [number_of_seeds_wanted]")
 
